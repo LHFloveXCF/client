4848695bb6dc31fc564e9cb4c595cd69988e2919
LHFloveXCF <826639991@qq.com>
2024-09-26 23:14:58 +0800

    记账本 计算选择月份的总账单

--- react-router-bill-pro/src/pages/Month/index.js
+++ react-router-bill-pro/src/pages/Month/index.js
@@ -11,20 +11,35 @@ const Month = () => {
 
     const {billList} = useSelector(state => state.bills)
     const monthGroup = useMemo(() => {
-      return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY | M '))
+      return _.groupBy(billList, (item) => dayjs(item.date).format('YYYY | M'))
     }, [billList])
-    console.log(monthGroup);
-    
+
+
+    const [currentGroup, setCurrentGroup] = useState([])
+
+    // console.log(monthGroup);
 
     const [dateNow, setDate] = useState(() => {
-        return dayjs().format('YYYY | M ')
+        return dayjs().format('YYYY | M')
     })
 
     function onConfirmDate(date) {
         setState(false)
+        const formatDate = dayjs(date).format('YYYY | M')
+        setDate(formatDate)
 
-        setDate(dayjs(date).format('YYYY | M '))
+        setCurrentGroup(monthGroup[formatDate] ? monthGroup[formatDate] : [])
     }
+
+    const currentResult = useMemo(() => {
+      const pay = currentGroup.filter(item => item.type === 'pay').reduce((a, c) => a + c.money, 0)
+      const get = currentGroup.filter(item => item.type === 'income').reduce((a, c) => a + c.money, 0)
+      return {
+        pay, get, total : pay + get 
+      }
+    }, [currentGroup])
+
+    // console.log(currentResult);
 
   return (
     <div className="monthlyBill">
@@ -36,22 +51,22 @@ const Month = () => {
           {/* 时间切换区域 */}
           <div className="date" onClick={() => setState(true)}>
             <span className="text">
-              {dateNow + ''}账单
+              {dateNow}账单
             </span>
             <span className= {classNames('arrow', state && 'expand')}></span>
           </div>
           {/* 统计区域 */}
           <div className='twoLineOverview'>
             <div className="item">
-              <span className="money">{100}</span>
+              <span className="money">{currentResult.pay.toFixed(2)}</span>
               <span className="type">支出</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{currentResult.get.toFixed(2)}</span>
               <span className="type">收入</span>
             </div>
             <div className="item">
-              <span className="money">{200}</span>
+              <span className="money">{currentResult.total.toFixed(2)}</span>
               <span className="type">结余</span>
             </div>
           </div>
