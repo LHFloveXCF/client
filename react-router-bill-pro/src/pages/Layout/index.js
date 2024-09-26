a41bdd55939f6fc2705cbad2d71b0fa182d3cc67
xuchaofeng <xuchaofeng@yoozoo.com>
2024-09-26 12:01:27 +0800

    记账本 tabBar功能实现

--- react-router-bill-pro/src/pages/Layout/index.js
+++ react-router-bill-pro/src/pages/Layout/index.js
@@ -2,6 +2,31 @@ import { getBillList } from "@/store/modules/billListStore"
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { Outlet } from "react-router-dom"
+import { TabBar } from "antd-mobile"
+import './index.scss'
+import {
+    BillOutline,
+    CalculatorOutline,
+    AddCircleOutline
+  } from 'antd-mobile-icons'
+
+  const tabs = [
+    {
+      key: '/month',
+      title: '月度账单',
+      icon: <BillOutline />,
+    },
+    {
+      key: '/new',
+      title: '记账',
+      icon: <AddCircleOutline />,
+    },
+    {
+      key: '/year',
+      title: '年度账单',
+      icon: <CalculatorOutline />,
+    },
+  ]
 
 const Layout = () => {
     
@@ -13,10 +38,18 @@ const Layout = () => {
     }, [dispatch])
 
     return (
-        <div>
-            <Outlet />
-            layout
-        </div>
+        <div className="layout">
+      <div className="container">
+        <Outlet />
+      </div>
+      <div className="footer">
+        <TabBar>
+          {tabs.map(item => (
+            <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+          ))}
+        </TabBar>
+      </div>
+    </div>
     )
 }
 
